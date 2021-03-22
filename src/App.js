8e216009f8d325247a4fafdb94ea71415856dfd8
rizwanullah313 <ru323091@gmail.com>
2021-03-22 09:35:05 -0700

    Material UI using in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, TextField, Button, CircularProgress } from '@material-ui/core';
+import NavBar from './NavBar';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: 400,
+    margin: '0 auto',
+    marginTop: 50,
+    textAlign: 'center',
+    padding: 15
+  },
+  input: {
+    width: '95%',
+    marginBottom: 10
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+        <NavBar />
+      <Paper elevation={3} className={classes.paper}>
+        <h1>Login</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField label="Enter Email" className={classes.input}/>
+          <TextField label="Enter Password" className={classes.input}/>
+          <Button variant="contained" color="primary" className={classes.input}>
+            Login
+          </Button>
+        </form>
+        <CircularProgress value={50} />
+      </Paper> 
     </div>
   );
 }
